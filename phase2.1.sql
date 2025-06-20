6aa0812f29f76f23831e48ab3ad8b79f9586456b
blanktomgaming <61638601+blanktomgaming@users.noreply.github.com>
2025-06-20 13:37:53 +0200

    Update phase2.1.sql

--- phase2.1.sql
+++ phase2.1.sql
@@ -17,8 +17,8 @@ CREATE TABLE  IF NOT EXISTS Employee (
 	experience INT NOT NULL,
 	hired DATE NOT NULL,
 	salary DECIMAL(10, 2) NOT NULL,
-	foreign KEY (username) REFERENCES User(username),
-	primary key(username, tax_id)
+	FOREIGN KEY (username) REFERENCES User(username),
+	PRIMARY KEY (username, tax_id)
 );
 
 
@@ -28,26 +28,26 @@ CREATE TABLE IF NOT EXISTS Worker (
 );
 
 CREATE TABLE IF NOT EXISTS Driver(
-	username VARCHAR(40) PRIMARY KEY,
-	licenseID CHAR(6) NOT NULL UNIQUE,
-	successful_trips INT NOT NULL,
-	license_type VARCHAR(5) NOT NULL,
-	FOREIGN KEY (username) REFERENCES User(username)
+	username VARCHAR(40),
+	licenseID CHAR(6),
+	successful_trips INT,
+	license_type VARCHAR(5),
+	FOREIGN KEY (username) REFERENCES User(username),
+	PRIMARY KEY (username, licenseID)
 );
-
-CREATE TABLE IF NOT EXISTS Service(
-	id varchar(40) PRIMARY KEY,
-	name varchar(40) NOT NULL,
-	location varchar(40) NOT NULL,
-	FOREIGN KEY (location) REFERENCES Location(location)
-);
-
 
 CREATE TABLE IF NOT EXISTS Location(
 	label varchar(40) PRIMARY KEY,
 	x_coord INT NOT NULL,
 	y_coord INT NOT NULL,
-	space INT NOT NULL,
+	space INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Service(
+	id varchar(40) PRIMARY KEY,
+	name varchar(40) NOT NULL,
+	location_label varchar(40) NOT NULL,
+	FOREIGN KEY (location_label) REFERENCES Location(label)
 );
 
 CREATE TABLE IF NOT EXISTS Product(
@@ -59,13 +59,13 @@ CREATE TABLE IF NOT EXISTS Product(
 );
 
 CREATE TABLE IF NOT EXISTS Van(
-	tag varchar(40),
-	service_id varchar(40),
+	tag varchar(40) NOT NULL,
+	service_id varchar(40) NOT NULL,
 	capacity int,
 	sales int,
 	fuel int,
-	foreign key (service_id) references Service(id),
-	primary key(tag, service_id)
+	FOREIGN KEY (service_id) REFERENCES Service(id),
+	PRIMARY KEY (tag, service_id)
 );
 #
 CREATE TABLE IF NOT EXISTS Product_contain_van(
@@ -74,47 +74,44 @@ CREATE TABLE IF NOT EXISTS Product_contain_van(
 	service_id varchar(40),
 	price int,
 	quantity int,
-	foreign key (barcode) references Product(barcode),
-	foreign key (tag, service_id) references Van(tag, service_id),
-	primary key(barcode, tag, service_id)
+	FOREIGN KEY (barcode) REFERENCES Product(barcode),
+	FOREIGN KEY (tag, service_id) REFERENCES Van(tag, service_id),
+	PRIMARY KEY (barcode, tag, service_id)
 );
 
 #ALTER TABLE Product ADD CONSTRAINT foreign key (label) Location(label);
 
 CREATE TABLE IF NOT EXISTS Owner (
-	username varchar(40),
-	primary key(username),
-	foreign key (username) references User(username)
+	username varchar(40) PRIMARY KEY,
+	FOREIGN KEY (username) REFERENCES User(username)
 );
 
 
 CREATE TABLE IF NOT EXISTS Business(
-	name varchar(40) NOT NULL UNIQUE,
+	name varchar(40) PRIMARY KEY,
 	rating int,
-	location_label varchar(40),
-	unique(name),
-	check(rating <= 5),
-	primary key(name),
-	foreign key (location_label) references Location(label)
+	location_label varchar(40) NOT NULL,
+	CHECK (rating <= 5),
+	FOREIGN KEY (location_label) REFERENCES Location(label)
 );
 
 CREATE TABLE IF NOT EXISTS Fund (
 	owner_username varchar(40),
-    business_name varchar(40),
-    invested float,
-    date date,
-    primary key(owner_username, business_name),
-    foreign key (owner_username) references Owner(username),
-    Foreign key (business_name) references Business(name)
+    	business_name varchar(40),
+    	invested float,
+    	date date,
+    	PRIMARY KEY (owner_username, business_name),
+    	FOREIGN KEY (owner_username) REFERENCES Owner(username),
+    	FOREIGN KEY (business_name) REFERENCES Business(name)
 );
 
 #
 
 CREATE TABLE IF NOT EXISTS Worker_workfor_service(
 	serviceID varchar(40),
-    tax_id varchar(40),
-    primary key(serviceID, tax_id),
-    foreign key (serviceID) references Service(ID),
-    foreign key (tax_id) references Employee(tax_id)
+    	tax_id varchar(40),
+    	PRIMARY KEY (serviceID, tax_id),
+    	FOREIGN KEY (serviceID) REFERENCES Service(ID),
+    	FOREIGN KEY (tax_id) REFERENCES Employee(tax_id)
 );
 
